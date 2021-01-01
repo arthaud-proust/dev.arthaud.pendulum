2c31396ade56acd7acf497f8722aeb2a553b4098
Arthaud Proust <arthaudp33@gmail.com>
2021-01-01 19:35:06 +0100

    v2

--- lever.js
+++ lever.js
@@ -1,7 +1,7 @@
 class Pendul {
     loadConfigUrl() {
         let params = {};
-        let toLoad=['phi1', 'phi2', 'm1', 'm2', 'l1', 'l2', 'tc', 'tw', 't', 'rc', 'dt', 'dl', 'lc']
+        let toLoad=['phi1', 'phi2', 'm1', 'm2', 'l1', 'l2', 'tc', 'tw', 'tl', 't', 'rc', 'dt', 'dl', 'lc']
         let parser = document.createElement('a');
         parser.href = window.location.href;
         let query = parser.search.substring(1);
@@ -21,24 +21,77 @@ class Pendul {
         });
     }
     getConfigUrl() {
-        let toLoad=['m1', 'm2', 'l1', 'l2', 'tc', 'tw', 't', 'rc', 'dt', 'dl', 'lc']
-        let url = window.location.host+'/?';
+        let toLoad=['m1', 'm2', 'l1', 'l2', 'tc', 'tw', 'tl', 't', 'rc', 'dt', 'dl', 'lc']
+        let host = window.location.host;
+        let path = '/?'
         toLoad.forEach((param, index)=>{
-            if(index>0) url+='&'
+            if(index>0) path+='&'
             if(param == "tc" || param == "lc") {
-                url+= `${param}=${this[param].replace('#','')}`;
+                path+= `${param}=${this[param].replace('#','')}`;
             } else if(param.includes('phi')) {
-                url+= `${param}=${Math.round(this[param]*2/(Math.PI))}`;
+                path+= `${param}=${Math.round(this[param]*2/(Math.PI))}`;
             } else {
-                url+= `${param}=${this[param]}`;
+                path+= `${param}=${this[param]}`;
             }
         });
-        return url;
+        return {
+            url:host+path,
+            host,
+            path
+        }
     }
 
     RGB2Color(r,g,b) {
-      return 'rgb(' + Math.round(r) + ',' + Math.round(g) + ',' + Math.round(b) + ')';
+        return 'rgb(' + Math.round(r) + ',' + Math.round(g) + ',' + Math.round(b) + ')';
     }
+    HEX2RGB(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? [
+          parseInt(result[1], 16),
+          parseInt(result[2], 16),
+          parseInt(result[3], 16)
+        ] : null;
+    }
+    HSL2RGB(h, s, l){
+        var r, g, b;
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            var hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+    
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+    
+        return [
+            Math.round(r * 255), 
+            Math.round(g * 255), 
+            Math.round(b * 255)
+        ];
+    }
+
+    COLOR2RGB(color) {
+        if(color.includes('hsl')) {
+            return this.HSL2RGB(color);
+        } else if(color.includes('#')) {
+            return this.HEX2RGB(color);
+        } else if(color.includes('rgb') && !color.includes('rgba')) {
+            return color.replace(/rgb\(|\)/g, '').split(',')
+        } else {
+            return('rgb(248, 168, 248)')
+        }
+    }
+
     constructor(name, height, width) {
         this.name = name;
         this.w = width;
@@ -61,13 +114,16 @@ class Pendul {
             this.l2 = ((this.w<this.h)?this.w:this.h)/6;
         }
         this.dt = true; //draw trail
-        this.dl = true; //draw lever
-        this.tc="#F8A8F8"; // trail color
+        this.dl = false; //draw lever
+        this.tc='#F8A8F8'; // trail color
         this.lc='#99CC99'; // lever color
-        this.tw=0.5; // trail width
+        this.tw=7.6; // trail width
+        this.tl=100; // trail length
         this.rc=false; // rainbow trail color
         this.t = 0.05;
 
+        // trail
+        this.trail = []
 
         // Coordonnées du haut du levier
         this.X0 = this.w/2; 
@@ -84,6 +140,19 @@ class Pendul {
         }
 
         this.loadConfigUrl();
+
+    }
+
+    get tcRGB() {
+        return this.COLOR2RGB(this.tc);
+    }
+
+    get actPos() {
+        return this._circle2 || {
+            x: this.X0+this.l1*Math.sin(this.phi1)+this.l2*Math.sin(this.phi2),
+            y: this.Y0+this.l1*Math.cos(this.phi1)+this.l2*Math.cos(this.phi2),
+            mass: this.m2
+        }
     }
 
     init(elParent) {
@@ -117,6 +186,8 @@ class Pendul {
                         <input id="t" class="pendul-input-t" type="range" min="0" max="12" value="${this.t*100}" step="2">
                         <label for="tw">Largeur de la trainée</label>
                         <input id="tw" class="pendul-input-tw" type="range" min="10" max="1010" value="${this.tw*100}" step="50">
+                        <label for="tl">Longueur de la trainée</label>
+                        <input id="tl" class="pendul-input-tl" type="range" min="10" max="510" value="${this.tl}" step="10">
                         <label for="lc">Couleur du levier</label>
                         <input id="lc" class="pendul-input-lc" type="color" value="${this.lc}">
                         <label for="tc">Couleur de la trainée</label>
@@ -125,13 +196,14 @@ class Pendul {
                     </section>
                     <section>
                         <h3>Plus</h3>
-                        <button class="shareBtn">Partager</a>
-                        <button class="disableTrail" ${this.dt?'':'style="display:none;"'}>Enlever la trainée</a>
-                        <button class="enableTrail" ${this.dt?'style="display:none;"':''}>Ajouter la trainée</a>
-                        <button class="disableLever" ${this.dl?'':'style="display:none;"'}>Cacher le levier</a>
-                        <button class="enableLever" ${this.dl?'style="display:none;"':''}>Afficher le levier</a>
-                        <button class="enableRainbowColor" ${this.rc?'style="display:none;"':''}>Couleur normale</a>
-                        <button class="disableRainbowColor" ${this.rc?'':'style="display:none;"'}>Couleurs arc-en-ciel</a>
+                        <button class="shareBtn">Partager</button>
+                        <button class="disableTrail" ${this.dt?'':'style="display:none;"'}>Enlever la trainée</button>
+                        <button class="enableTrail" ${this.dt?'style="display:none;"':''}>Ajouter la trainée</button>
+                        <button class="disableLever" ${this.dl?'':'style="display:none;"'}>Cacher le levier</button>
+                        <button class="enableLever" ${this.dl?'style="display:none;"':''}>Afficher le levier</button>
+                        <button class="enableRainbowColor" ${this.rc?'style="display:none;"':''}>Couleur normale</button>
+                        <button class="disableRainbowColor" ${this.rc?'':'style="display:none;"'}>Couleurs arc-en-ciel</button>
+                        <a href="/" class="button reset">Réinitialiser</a>
                     </section>
                     <div id="credit">Développé par <a href="https://arthaud.dev">Arthaud Proust</a> &copy2021</div>
                 </div>
@@ -144,6 +216,10 @@ class Pendul {
             </div>
         `);
 
+        $('#pendul-control').on('click', ()=> {
+            window.history.replaceState('Modification', 'Pendulum', this.getConfigUrl().path);
+        })
+
         $(`#${this.name}-control .pendul-input-phi`).on('change', (e)=>{
             clearInterval(this.runAnim);
             this[$(e.target).attr("id")] = $(e.target).val()/180*(Math.PI);
@@ -151,6 +227,9 @@ class Pendul {
         });
         $(`#${this.name}-control .pendul-input-t, #${this.name}-control .pendul-input-tw`).on('change', (e)=>{
             this[$(e.target).attr("id")] = $(e.target).val()/100;
+        });
+        $(`#${this.name}-control .pendul-input-tl`).on('change', (e)=>{
+            this[$(e.target).attr("id")] = $(e.target).val();
         });
         $(`#${this.name}-control .pendul-input-l`).on('change', (e)=>{
             this[$(e.target).attr("id")] = $(e.target).val();
@@ -172,8 +251,7 @@ class Pendul {
         });
         $('.shareBtn').click((e)=>{
             $(e.target).closest('.pendul-bloc').find('.pendul-sharePopup').fadeIn();
-            $(e.target).closest('.pendul-bloc').find('.pendul-sharePopup-link').text(this.getConfigUrl());
-            
+            $(e.target).closest('.pendul-bloc').find('.pendul-sharePopup-link').text(this.getConfigUrl().url);
         })
         $('.pendul-sharePopup-close').click((e)=>{
             $(e.target).closest('.pendul-sharePopup').fadeOut();
@@ -210,7 +288,6 @@ class Pendul {
             $(e.target).fadeOut(()=>{
                 $(e.target).prev('.disableLever').fadeIn();
             });
-            
         })
         $('.disableRainbowColor').click((e)=>{
             this.rc = false;
@@ -238,8 +315,8 @@ class Pendul {
                 $(e.target).css("background", "url('/assets/close.svg') no-repeat");
                 
             }
-            
         })
+
         this.cvs = $('#'+this.name);
         this.cvsTrail = $('#'+this.name+'-trail');
     }
@@ -290,7 +367,10 @@ class Pendul {
         circle1.y = this.Y0+this.l1*Math.cos(this.phi1);
         circle2.x = this.X0+this.l1*Math.sin(this.phi1)+this.l2*Math.sin(this.phi2);
         circle2.y = this.Y0+this.l1*Math.cos(this.phi1)+this.l2*Math.cos(this.phi2);
-      
+        
+        this._circle2 = circle2;
+
+
         lever1.x  = circle1.x;
         lever1.y  = circle1.y;
         lever2.x0 = circle1.x;
@@ -300,38 +380,91 @@ class Pendul {
       
         this.cvs.clearCanvas()
 
+        // point du millieu
         this.cvs.drawArc({
             fillStyle: '#5D5D60',
             x: this.X0, y: this.Y0,
             radius: 7
         });
+
         this.drawLever(lever1);
         this.drawLever(lever2);
         this.drawCircle(circle1);
         this.drawCircle(circle2);
+
+        this.drawTrail();
+    }
+
+    getTrailStepColor(i) {
+        if(this.rc) {
+            let { center, width, frequency, phase } = this.rainbowParams;
+            let red = Math.sin(frequency*i+2+phase) * width + center;
+            let green = Math.sin(frequency*i+0+phase) * width + center;
+            let blue = Math.sin(frequency*i+4+phase) * width + center;
+
+            return [red,green,blue]
+        } else {
+            return this.tcRGB
+        }
+    }
+
+    getAlphaColor(indexOfTrail, i) {
+        let [r, g, b] = this.getTrailStepColor(i);
+        let alpha = indexOfTrail/this.tl;
+        return `rgba(${r},${g},${b},${alpha})`
+    }
+
+    drawTrail() {
+        if(this.trail.length==this.tl) this.trail.shift();
+        if(this.trail.length>this.tl) this.trail = this.trail.splice(0, this.tl-1);
+        this.rainbowParams.i++;
+        
+        this.trail.push([
+            this.actPos.x, 
+            this.actPos.y,
+            this.rainbowParams.i
+        ]);
+
         if(this.dt) {
+            /*
             if(this.rc) {
                 let { center, width, frequency, phase, i } = this.rainbowParams;
                 let red = Math.sin(frequency*i+2+phase) * width + center;
                 let green = Math.sin(frequency*i+0+phase) * width + center;
                 let blue = Math.sin(frequency*i+4+phase) * width + center;
-                this.cvsTrail.drawArc({
-                    fillStyle: this.RGB2Color(red,green,blue),
-                    x: circle2.x, 
-                    y: circle2.y, 
-                    radius: this.tw
-                });
+                // this.cvsTrail.drawArc({
+                //     fillStyle: this.RGB2Color(red,green,blue),
+                //     x: this.actPos.x, 
+                //     y: this.actPos.y, 
+                //     radius: this.tw
+                // });
                 this.rainbowParams.i++;
             } else {
+                // this.cvsTrail.drawArc({
+                //     fillStyle: this.tc,
+                //     x: this.actPos.x, 
+                //     y: this.actPos.y, 
+                //     radius: this.tw
+                // });
+                this.trail.push([
+                    this.actPos.x, 
+                    this.actPos.y
+                ]);
+            }*/
+
+            this.cvsTrail.clearCanvas()
+            for(let i=0; i<this.trail.length; i++) {
                 this.cvsTrail.drawArc({
-                    fillStyle: this.tc,
-                    x: circle2.x, 
-                    y: circle2.y, 
+                    fillStyle: this.getAlphaColor(i, this.trail[i][2]),
+                    x: this.trail[i][0], 
+                    y: this.trail[i][1], 
                     radius: this.tw
                 });
             }
-            
         }
+
+
+        
     }
 
     drawLever(line) {
@@ -354,9 +487,26 @@ class Pendul {
             radius: circle.mass
         });
     }
+
+    enableLever() {
+        console.log('e');
+        this.dl = true;
+        $(`#${this.name}`).animate({
+            opacity:1
+        }, 2000);
+        $(`.${this.name}-control .enableLever`).fadeOut(()=>{
+            $(`.${this.name}-control .enableLever`).prev('.disableLever').fadeIn();
+        });
+    }
 }
 
 
 const chaotic = new Pendul("pendul", $(document).height(), $(document).width());
 chaotic.init("body");
 chaotic.run();
+
+if(location.search=="") {
+    setTimeout(function() {
+        chaotic.enableLever()
+    }, 2000)
+}
